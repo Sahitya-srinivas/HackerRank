8f3d4a0578800fa61fcf1abb77ab6af7fc20122d
sahitya-priya-d <dpriya@galaxe.com>
2025-04-16 14:31:33 +0530

    Day 1

---
+++ 1 Week Preparation Kit/Day1/Time Conversion.java
@@ -0,0 +1,21 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+class Result{
+    public static String timeConversion(String s){
+        DateTimeFormatter input = DateTimeFormatter.ofPattern("hh:mm:ssa");
+        DateTimeFormatter output = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime time = LocalTime.parse(s, input);
+        return time.format(output);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String result = Result.timeConversion(s);
+        System.out.println(result);
+    }
+}
