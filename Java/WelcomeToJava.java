22caebebd51210a2c439d16ef006972c4ff7a008
sahitya-priya-d <dpriya@galaxe.com>
2025-04-16 16:28:06 +0530

    Practice

---
+++ Java/WelcomeToJava.java
@@ -0,0 +1,12 @@
+/*You must print two lines of output:
+	1. Print Hello, World. on the first line.
+	2. Print Hello, Java. on the second line.
+ */
+
+public class WelcomeToJava {
+	public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
